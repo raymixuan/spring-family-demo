00415d73f816bf2889b13a0e1f5a5d8b4f4407f4
raymixuan <raymixuan@users.noreply.github.com>
2019-03-13 00:23:45 +0800

    注册中心初始化

---
+++ cloud-eureka/src/main/java/cn/oldriver/cloud/euraka/Application.java
@@ -0,0 +1,20 @@
+package cn.oldriver.cloud.euraka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * @Author kejx
+ * @Date 2019/3/13 0:16
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
